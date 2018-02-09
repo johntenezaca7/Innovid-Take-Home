2462bf21ae43bc5d79165d67faa9602af3ff9d7b
Owner <owner@MacBook-Pro.local>
2018-02-09 13:48:42 -0500

    react set up with express server

---
+++ client/react-front-end/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                Best Buy
+            </div>
+        )
+    }
+}
+
+export default App;
